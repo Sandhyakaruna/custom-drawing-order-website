24bc25241bbb1e9a92755baa51a0ceffa358109d
Sandhya K <115928773+Sandhyakaruna@users.noreply.github.com>
2024-10-20 22:41:32 +0530

    Create DrawingOrderRepository.java

---
+++ DrawingOrderRepository.java
@@ -0,0 +1,6 @@
+package com.example.drawings;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DrawingOrderRepository extends JpaRepository<DrawingOrder, Long> {
+}
